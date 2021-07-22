26364caea85d9eaac4c23e8d9b21d54a55b61dae
sol <safe@vmi504578.contaboserver.net>
2021-07-22 19:36:42 +0200

    clear

--- sdk/src/rpc_port.rs
+++
@@ -1,9 +0,0 @@
-/// Default port number for JSON RPC API
-pub const DEFAULT_RPC_PORT: u16 = 8328;
-pub const DEFAULT_RPC_PORT_STR: &str = "8328";
-
-/// Default port number for JSON RPC pubsub
-pub const DEFAULT_RPC_PUBSUB_PORT: u16 = 8329;
-
-/// Default port number for Banks RPC API
-pub const DEFAULT_RPC_BANKS_PORT: u16 = 8901;
