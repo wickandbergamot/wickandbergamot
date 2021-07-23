4c8995ec1ebc4df85eb07fe6351f533acd9955e1
sol <safe@vmi504578.contaboserver.net>
2021-07-23 07:34:58 +0200

    clear

--- explorer/src/components/common/Slot.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-type Props = {
-  slot: number;
-};
-export function Slot({ slot }: Props) {
-  return <span className="text-monospace">{slot.toLocaleString("en-US")}</span>;
-}
--- explorer/src/providers/stats/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { SafecoinBeachProvider } from "./solanaBeach";
-
-type Props = { children: React.ReactNode };
-export function StatsProvider({ children }: Props) {
-  return <SafecoinBeachProvider>{children}</SafecoinBeachProvider>;
-}
