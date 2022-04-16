4a39b1208af6e62ec5c3c503e9cf148efe24e8a4
Ron Swanson <testnet@vmi509528.contaboserver.net>
2022-04-16 20:12:16 +0200

    Revert "replace sad (non-standard) hash with new happy (standard) hash"
    This reverts commit e8a75e208a377dc052ccb0615f652e40cb616466.

--- runtime/src/vote_group_gen.rs
+++ runtime/src/vote_group_gen.rs
@@ -7,18 +7,15 @@
 use safecoin_sdk::{
     pubkey::Pubkey,
     hash::Hash,
-    hash::extend_and_hash,
 };
-
 use std::collections::HashMap;
 use std::convert::TryInto;
 use std::str::FromStr;
 
 pub static SAFECOIN_ALWAYS_VOTER: &str = "83E5RMejo6d98FV1EAXTx5t4bvoDMoxE4DboDee3VJsu";
 
-pub fn rand_voter_hash( hash: Hash, test_key: Pubkey) -> Hash{
-    extend_and_hash(&hash,  &test_key.to_bytes())
-}
+//#[derive(Clone, Debug, Serialize, Deserialize, AbiExample, PartialEq)]
+//pub struct ArcPubkey(std::sync::Arc<Pubkey>);
 
 #[derive(Clone, Debug, Serialize, Deserialize, AbiExample, PartialEq)]
 pub struct VoteGroupGenerator {
