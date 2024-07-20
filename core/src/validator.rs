be1e1e8e918fa614aff52ad87ebee9054467ee49
Ron Swanson <98294584+jeffowy@users.noreply.github.com>
2024-07-20 15:30:25 -0600

    Supermajority 60%

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -115,7 +115,7 @@ use {
 };
 
 const MAX_COMPLETED_DATA_SETS_IN_CHANNEL: usize = 100_000;
-const WAIT_FOR_SUPERMAJORITY_THRESHOLD_PERCENT: u64 = 50;
+const WAIT_FOR_SUPERMAJORITY_THRESHOLD_PERCENT: u64 = 60;
 
 pub struct ValidatorConfig {
     pub halt_at_slot: Option<Slot>,
