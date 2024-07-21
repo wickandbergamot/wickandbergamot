554c1484281f7cfa9ad9a3bfa1655ee15b134105
jeffowy <fair-exchange@protonmail.ch>
2024-07-21 18:08:14 +0200

    temporary consensus revert

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -2052,7 +2052,7 @@ impl ReplayStage {
 	       	 + authorized_voter_pubkey.to_string().chars().last().unwrap() as usize
 	       	 + vote.hash().to_string().chars().last().unwrap() as usize ) % 10 as usize;
 
-let allowed_offset_int = 0;
+let allowed_offset_int = 10;
 
 //  Compare generated integers to determine voter selection.   Given method has	a 3/10 chance, plus bootstrap option
 if (slot_hash_int > (mixed_int + allowed_offset_int) ||  slot_hash_int < (mixed_int - allowed_offset_int))
