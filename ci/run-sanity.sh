124b4c8b8ad6a23f0760eb29fd44ad750b581b6e
wick&bergamot <sarahpoonn@gmail.com>
2024-08-12 18:12:04 +0800

    Update run-sanity.sh

--- ci/run-sanity.sh
+++ ci/run-sanity.sh
@@ -7,7 +7,7 @@ source multinode-demo/common.sh
 
 rm -rf config/run/init-completed config/ledger config/snapshot-ledger
 
-SAFECOIN_RUN_SH_VALIDATOR_ARGS="--full-snapshot-interval-slots 200" timeout 120 ./scripts/run.sh &
+WICKANDBERGAMOT_RUN_SH_VALIDATOR_ARGS="--full-snapshot-interval-slots 200" timeout 120 ./scripts/run.sh &
 pid=$!
 
 attempts=20
@@ -28,15 +28,15 @@ latest_slot=0
 while [[ $latest_slot -le $((snapshot_slot + 1)) ]]; do
   sleep 1
   echo "Checking slot"
-  latest_slot=$($safecoin_cli --url http://localhost:8328 slot --commitment processed)
+  latest_slot=$($wickandbergamot_cli --url http://localhost:8328 slot --commitment processed)
 done
 
-$safecoin_validator --ledger config/ledger exit --force || true
+$wickandbergamot_validator --ledger config/ledger exit --force || true
 
 wait $pid
 
-$safecoin_ledger_tool create-snapshot --ledger config/ledger "$snapshot_slot" config/snapshot-ledger
+$wickandbergamot_ledger_tool create-snapshot --ledger config/ledger "$snapshot_slot" config/snapshot-ledger
 cp config/ledger/genesis.tar.bz2 config/snapshot-ledger
-$safecoin_ledger_tool copy --ledger config/ledger \
+$wickandbergamot_ledger_tool copy --ledger config/ledger \
   --target-db config/snapshot-ledger --starting-slot "$snapshot_slot" --ending-slot "$latest_slot"
-$safecoin_ledger_tool verify --ledger config/snapshot-ledger
+$wickandbergamot_ledger_tool verify --ledger config/snapshot-ledger
