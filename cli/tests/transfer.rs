ba661414ee6d52dd5393f9c1b244f1b5ac9be3fa
wick&bergamot <sarahpoonn@gmail.com>
2024-08-13 11:01:48 +0800

    Update transfer.rs

--- cli/tests/transfer.rs
+++ cli/tests/transfer.rs
@@ -1,19 +1,19 @@
 #![allow(clippy::integer_arithmetic)]
 #![allow(clippy::redundant_closure)]
 use {
-    safecoin_cli::{
+    wickandbergamot_cli::{
         check_balance,
         cli::{process_command, request_and_confirm_airdrop, CliCommand, CliConfig},
         spend_utils::SpendAmount,
         test_utils::check_ready,
     },
-    safecoin_cli_output::{parse_sign_only_reply_string, OutputFormat},
-    safecoin_client::{
+    wickandbergamot_cli_output::{parse_sign_only_reply_string, OutputFormat},
+    wickandbergamot_client::{
         blockhash_query::{self, BlockhashQuery},
         nonce_utils,
         rpc_client::RpcClient,
     },
-    safecoin_faucet::faucet::run_local_faucet,
+    wickandbergamot_faucet::faucet::run_local_faucet,
     solana_sdk::{
         commitment_config::CommitmentConfig,
         fee::FeeStructure,
@@ -24,7 +24,7 @@ use {
         stake,
     },
     solana_streamer::socket::SocketAddrSpace,
-    safecoin_test_validator::TestValidator,
+    wickandbergamot_test_validator::TestValidator,
 };
 
 #[test]
