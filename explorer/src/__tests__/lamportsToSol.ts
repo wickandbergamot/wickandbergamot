b7ba7e7ff83a2813a3465996e3985cbac00eb371
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-14 21:04:26 +0800

    Update lamportsToSol.ts

--- explorer/src/__tests__/lamportsToSol.ts
+++ explorer/src/__tests__/lamportsToSol.ts
@@ -1,27 +1,27 @@
 import { expect } from "chai";
-import { lamportsToSafe, LAMPORTS_PER_SAFE } from "utils";
+import { lamportsToWickandbergamot, LAMPORTS_PER_WICKANDBERGAMOT } from "utils";
 import BN from "bn.js";
 
-describe("lamportsToSafe", () => {
+describe("lamportsToWICKANDBERGAMOT", () => {
   it("0 lamports", () => {
-    expect(lamportsToSafe(new BN(0))).to.eq(0.0);
+    expect(lamportsToWickandbergamot(new BN(0))).to.eq(0.0);
   });
 
   it("1 lamport", () => {
-    expect(lamportsToSafe(new BN(1))).to.eq(0.000000001);
-    expect(lamportsToSafe(new BN(-1))).to.eq(-0.000000001);
+    expect(lamportsToWickandbergamot(new BN(1))).to.eq(0.000000001);
+    expect(lamportsToWickandbergamot(new BN(-1))).to.eq(-0.000000001);
   });
 
-  it("1 SAFE", () => {
-    expect(lamportsToSafe(new BN(LAMPORTS_PER_SAFE))).to.eq(1.0);
-    expect(lamportsToSafe(new BN(-LAMPORTS_PER_SAFE))).to.eq(-1.0);
+  it("1 WICKANDBERGAMOT", () => {
+    expect(lamportsToWickandbergamot(new BN(LAMPORTS_PER_WICKANDBERGAMOT))).to.eq(1.0);
+    expect(lamportsToWickandbergamot(new BN(-LAMPORTS_PER_WICKANDBERGAMOT))).to.eq(-1.0);
   });
 
   it("u64::MAX lamports", () => {
-    expect(lamportsToSafe(new BN(2).pow(new BN(64)))).to.eq(
+    expect(lamportsToWickandbergamot(new BN(2).pow(new BN(64)))).to.eq(
       18446744073.709551615
     );
-    expect(lamportsToSafe(new BN(2).pow(new BN(64)).neg())).to.eq(
+    expect(lamportsToWickandbergamot(new BN(2).pow(new BN(64)).neg())).to.eq(
       -18446744073.709551615
     );
   });
