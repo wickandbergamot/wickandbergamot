80102bc4b91957dc92f21590d63b91cc68af06e3
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-16 00:30:13 +0800

    Update ip_address_server.rs

--- net-utils/src/bin/ip_address_server.rs
+++ net-utils/src/bin/ip_address_server.rs
@@ -5,8 +5,8 @@ use {
 
 fn main() {
     solana_logger::setup();
-    let matches = Command::new("safecoin-ip-address-server")
-        .version(safecoin_version::version!())
+    let matches = Command::new("wickandbergamot-ip-address-server")
+        .version(wickandbergamot_version::version!())
         .arg(
             Arg::new("port")
                 .index(1)
@@ -21,7 +21,7 @@ fn main() {
         .unwrap_or_else(|_| panic!("Unable to parse {}", port));
     let bind_addr = SocketAddr::from(([0, 0, 0, 0], port));
     let tcp_listener = TcpListener::bind(bind_addr).expect("unable to start tcp listener");
-    let _runtime = safecoin_net_utils::ip_echo_server(tcp_listener, /*shred_version=*/ None);
+    let _runtime = wickandbergamot_net_utils::ip_echo_server(tcp_listener, /*shred_version=*/ None);
     loop {
         std::thread::park();
     }
