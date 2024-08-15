2602b9686e4c46a40be3d68330b521620bebbc2f
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-16 00:42:52 +0800

    Update add-datacenter-solana-user-authorized_keys.sh

--- net/scripts/add-datacenter-solana-user-authorized_keys.sh
+++ net/scripts/add-datacenter-solana-user-authorized_keys.sh
@@ -8,7 +8,7 @@ source solana-user-authorized_keys.sh
 
 # solana-user-authorized_keys.sh defines the public keys for users that should
 # automatically be granted access to ALL datacenter nodes.
-for i in "${!SAFECOIN_USERS[@]}"; do
-  echo "environment=\"SAFECOIN_USER=${SAFECOIN_USERS[i]}\" ${SAFECOIN_PUBKEYS[i]}"
+for i in "${!WICKANDBERGAMOT_USERS[@]}"; do
+  echo "environment=\"WICKANDBERGAMOT_USER=${WICKANDBERGAMOT_USERS[i]}\" ${WICKANDBERGAMOT_PUBKEYS[i]}"
 done
 
