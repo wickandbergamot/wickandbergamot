16ab68450ee82b2ca812f20aaf1c77928fd97ce5
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-15 14:21:36 +0800

    Update NonceWithdrawDetailsCard.tsx

--- explorer/src/components/instruction/system/NonceWithdrawDetailsCard.tsx
+++ explorer/src/components/instruction/system/NonceWithdrawDetailsCard.tsx
@@ -3,8 +3,8 @@ import {
   SystemProgram,
   SignatureResult,
   ParsedInstruction,
-} from "@safecoin/web3.js";
-import { SafeBalance } from "utils";
+} from "@wickandbergamot/web3.js";
+import { WickandbergamotBalance } from "utils";
 import { InstructionCard } from "../InstructionCard";
 import { Address } from "components/common/Address";
 import { WithdrawNonceInfo } from "./types";
@@ -57,9 +57,9 @@ export function NonceWithdrawDetailsCard(props: {
       </tr>
 
       <tr>
-        <td>Withdraw Amount (SAFE)</td>
+        <td>Withdraw Amount (WICKANDBERGAMOT)</td>
         <td className="text-lg-end">
-          <SafeBalance lamports={info.lamports} />
+          <WickandbergamotBalance lamports={info.lamports} />
         </td>
       </tr>
     </InstructionCard>
