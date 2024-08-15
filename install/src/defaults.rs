dab1827e3b75918d12ea262715f30060c3d2f0d7
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-15 21:56:18 +0800

    Update defaults.rs

--- install/src/defaults.rs
+++ install/src/defaults.rs
@@ -1,4 +1,4 @@
-pub const JSON_RPC_URL: &str = "http://api.devnet.safecoin.org";
+pub const JSON_RPC_URL: &str = "http://api.devnet.wickandbergamot.org";
 
 lazy_static! {
     pub static ref CONFIG_FILE: Option<String> = {
