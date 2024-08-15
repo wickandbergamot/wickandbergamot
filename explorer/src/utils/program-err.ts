6665ad54c5ef3f82a1739fd811854d295a3061fe
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-15 20:03:02 +0800

    Update program-err.ts

--- explorer/src/utils/program-err.ts
+++ explorer/src/utils/program-err.ts
@@ -1,4 +1,4 @@
-import { TransactionError } from "@safecoin/web3.js";
+import { TransactionError } from "@wickandbergamot/web3.js";
 
 const instructionErrorMessage: Map<string, string> = new Map([
   ["GenericError", "generic instruction error"],
