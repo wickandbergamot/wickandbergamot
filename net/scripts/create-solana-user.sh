98fa35064ba2a4188ca097c2bfcbe4303028e43c
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-16 00:50:00 +0800

    Update create-solana-user.sh

--- net/scripts/create-solana-user.sh
+++ net/scripts/create-solana-user.sh
@@ -4,19 +4,19 @@ set -ex
 [[ $(uname) = Linux ]] || exit 1
 [[ $USER = root ]] || exit 1
 
-if grep -q safecoin /etc/passwd ; then
-  echo "User safecoin already exists"
+if grep -q wickandbergamot /etc/passwd ; then
+  echo "User wickandbergamot already exists"
 else
-  adduser safecoin --gecos "" --disabled-password --quiet
-  adduser safecoin sudo
-  adduser safecoin adm
-  echo "safecoin ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+  adduser wickandbergamot --gecos "" --disabled-password --quiet
+  adduser wickandbergamot sudo
+  adduser wickandbergamot adm
+  echo "wickandbergamot ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
   id solana
 
   [[ -r /solana-scratch/id_ecdsa ]] || exit 1
   [[ -r /solana-scratch/id_ecdsa.pub ]] || exit 1
 
-  sudo -u safecoin bash -c "
+  sudo -u wickandbergamot bash -c "
     echo 'PATH=\"/home/solana/.cargo/bin:$PATH\"' > /home/solana/.profile
     mkdir -p /home/solana/.ssh/
     cd /home/solana/.ssh/
