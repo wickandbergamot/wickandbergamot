370844037be551702a67686a6da18db9a76eb206
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-16 00:21:24 +0800

    Update faucet.sh

--- multinode-demo/faucet.sh
+++ multinode-demo/faucet.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 #
-# Starts an instance of safecoin-faucet
+# Starts an instance of wickandbergamot-faucet
 #
 here=$(dirname "$0")
 
 # shellcheck source=multinode-demo/common.sh
 source "$here"/common.sh
 
-[[ -f "$SAFECOIN_CONFIG_DIR"/faucet.json ]] || {
-  echo "$SAFECOIN_CONFIG_DIR/faucet.json not found, create it by running:"
+[[ -f "$WICKANDBERGAMOT_CONFIG_DIR"/faucet.json ]] || {
+  echo "$WICKANDBERGAMOT_CONFIG_DIR/faucet.json not found, create it by running:"
   echo
   echo "  ${here}/setup.sh"
   exit 1
 }
 
 set -x
-# shellcheck disable=SC2086 # Don't want to double quote $safecoin_faucet
-exec $safecoin_faucet --keypair "$SAFECOIN_CONFIG_DIR"/faucet.json "$@"
+# shellcheck disable=SC2086 # Don't want to double quote $WICKANDBERGAMOT_faucet
+exec $wickandbergamot_faucet --keypair "$WICKANDBERGAMOT_CONFIG_DIR"/faucet.json "$@"
