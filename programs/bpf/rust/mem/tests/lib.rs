e001aaec61126104b9725a18af7eeeaf8549baa4
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-16 16:02:23 +0800

    Update lib.rs

--- programs/bpf/rust/mem/tests/lib.rs
+++ programs/bpf/rust/mem/tests/lib.rs
@@ -1,5 +1,5 @@
 use {
-    safecoin_bpf_rust_mem::entrypoint::process_instruction,
+    wickandbergamot_bpf_rust_mem::entrypoint::process_instruction,
     solana_program_test::*,
     solana_sdk::{
         instruction::Instruction, pubkey::Pubkey, signature::Signer, transaction::Transaction,
@@ -10,7 +10,7 @@ use {
 async fn test_mem() {
     let program_id = Pubkey::new_unique();
     let program_test = ProgramTest::new(
-        "safecoin_bpf_rust_mem",
+        "wickandbergamot_bpf_rust_mem",
         program_id,
         processor!(process_instruction),
     );
