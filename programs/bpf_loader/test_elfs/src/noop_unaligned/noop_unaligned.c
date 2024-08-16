04f7fcaa7c296ac7465fd087670f7ea66afcf952
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-16 17:48:22 +0800

    Update noop_unaligned.c

--- programs/bpf_loader/test_elfs/src/noop_unaligned/noop_unaligned.c
+++ programs/bpf_loader/test_elfs/src/noop_unaligned/noop_unaligned.c
@@ -7,10 +7,10 @@
 #include <sol/deserialize_deprecated.h>
 
 extern uint64_t entrypoint(const uint8_t *input) {
-  SafeAccountInfo ka[2];
-  SafeParameters params = (SafeParameters) { .ka = ka };
+  WickandbergamotAccountInfo ka[2];
+  WickandbergamotParameters params = (WickandbergamotParameters) { .ka = ka };
 
-  if (!sol_deserialize_deprecated(input, &params, SAFE_ARRAY_SIZE(ka))) {
+  if (!sol_deserialize_deprecated(input, &params, WICKANDBERGAMOT_ARRAY_SIZE(ka))) {
     return ERROR_INVALID_ARGUMENT;
   }
 
