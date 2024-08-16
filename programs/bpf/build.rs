06c2b2ead9e7586499caef9dc7364a4f2df5107f
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-16 17:32:08 +0800

    Update build.rs

--- programs/bpf/build.rs
+++ programs/bpf/build.rs
@@ -104,7 +104,7 @@ fn main() {
         ];
         for program in rust_programs.iter() {
             println!(
-                "cargo:warning=(not a warning) Building Rust-based BPF programs: safecoin_bpf_rust_{}",
+                "cargo:warning=(not a warning) Building Rust-based BPF programs: wickandbergamot_bpf_rust_{}",
                 program
             );
             assert!(Command::new("../../cargo-build-bpf")
