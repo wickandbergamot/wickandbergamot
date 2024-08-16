23d19b890ba493b3ad9a247594c6e5f7633a9e17
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-16 12:55:02 +0800

    Update return_data.c

--- programs/bpf/c/src/return_data/return_data.c
+++ programs/bpf/c/src/return_data/return_data.c
@@ -7,7 +7,7 @@
 
 extern uint64_t entrypoint(const uint8_t *input) {
   uint8_t buf[1024];
-  SafePubkey me;
+  WickandbergamotPubkey me;
 
   // There should be no return data on entry
   uint64_t ret = sol_get_return_data(NULL, 0, NULL);
