0806d08105cb64aefcd6c13c6b28097584ed62b6
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-16 16:04:52 +0800

    Update lib.rs

--- programs/bpf/rust/membuiltins/src/lib.rs
+++ programs/bpf/rust/membuiltins/src/lib.rs
@@ -5,7 +5,7 @@
 
 extern crate compiler_builtins;
 use {
-    safecoin_bpf_rust_mem::{run_mem_tests, MemOps},
+    wickandbergamot_bpf_rust_mem::{run_mem_tests, MemOps},
     solana_program::{custom_heap_default, custom_panic_default, entrypoint::SUCCESS},
 };
 
