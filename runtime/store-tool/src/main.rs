f7e65890480a84bb4a389b92bfe37b8a1a4e8287
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-20 07:44:09 +0800

    Update main.rs

--- runtime/store-tool/src/main.rs
+++ runtime/store-tool/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     solana_logger::setup_with_default("solana=info");
     let matches = App::new(crate_name!())
         .about(crate_description!())
-        .version(safecoin_version::version!())
+        .version(wickandbergamot_version::version!())
         .arg(
             Arg::with_name("file")
                 .long("file")
