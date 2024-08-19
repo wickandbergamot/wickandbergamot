1452aca1bbf3cd961fabd71eb0aaba60cd9bfbd6
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-20 07:35:10 +0800

    Update lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -39,9 +39,9 @@ pub mod execute_cost_table;
 pub mod genesis_utils;
 pub mod hardened_unpack;
 pub mod in_mem_accounts_index;
-pub mod inline_safe_associated_token_account;
-pub mod inline_safe_token;
-pub mod inline_safe_token_2022;
+pub mod inline_wickandbergamot_associated_token_account;
+pub mod inline_wickandbergamot_token;
+pub mod inline_wickandbergamot_token_2024;
 pub mod loader_utils;
 pub mod message_processor;
 pub mod non_circulating_supply;
@@ -88,4 +88,4 @@ extern crate solana_metrics;
 extern crate serde_derive;
 
 #[macro_use]
-extern crate safecoin_frozen_abi_macro;
+extern crate wickandbergamot_frozen_abi_macro;
