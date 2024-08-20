90a3c7883a14382e5d97cd781ff348390279c5a9
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-20 08:43:27 +0800

    Update mod.rs

--- sdk/program/src/message/mod.rs
+++ sdk/program/src/message/mod.rs
@@ -2,10 +2,10 @@
 //!
 //! [`Instruction`]: crate::instruction::Instruction
 //!
-//! In Safecoin, programs execute instructions, and clients submit sequences
+//! In Wickandbergamot, programs execute instructions, and clients submit sequences
 //! of instructions to the network to be atomically executed as [`Transaction`]s.
 //!
-//! [`Transaction`]: https://docs.rs/safecoin-sdk/latest/safecoin-sdk/transaction/struct.Transaction.html
+//! [`Transaction`]: https://docs.rs/wickandbergamot-sdk/latest/wickandbergamot-sdk/transaction/struct.Transaction.html
 //!
 //! A [`Message`] is the compact internal encoding of a transaction, as
 //! transmitted across the network and stored in, and operated on, by the
@@ -26,15 +26,15 @@
 //!
 //! This module defines two versions of `Message` in their own modules:
 //! [`legacy`] and [`v0`]. `legacy` is reexported here and is the current
-//! version as of Safecoin 1.10.0. `v0` is a [future message format] that encodes
+//! version as of wickandbergamot 1.10.0. `v0` is a [future message format] that encodes
 //! more account keys into a transaction than the legacy format. The
 //! [`VersionedMessage`] type is a thin wrapper around either message version.
 //!
 //! [future message format]: https://docs.solana.com/proposals/transactions-v2
 //!
-//! Despite living in the `safecoin-program` crate, there is no way to access the
-//! runtime's messages from within a Safecoin program, and only the legacy message
-//! types continue to be exposed to Safecoin programs, for backwards compatibility
+//! Despite living in the `wickandbergamot-program` crate, there is no way to access the
+//! runtime's messages from within a wickandbergamot program, and only the legacy message
+//! types continue to be exposed to wickandbergamot programs, for backwards compatibility
 //! reasons.
 
 mod compiled_keys;
