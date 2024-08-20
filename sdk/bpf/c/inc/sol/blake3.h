7e156852dc3c2e8cb9636c565178ef9cb1b9a902
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-20 08:11:04 +0800

    Update blake3.h

--- sdk/bpf/c/inc/sol/blake3.h
+++ sdk/bpf/c/inc/sol/blake3.h
@@ -1,6 +1,6 @@
 #pragma once
 /**
- * @brief Safecoin Blake3 system call
+ * @brief Wickandbergamot Blake3 system call
  */
 
 #include <sol/types.h>
@@ -22,11 +22,11 @@ extern "C" {
  * @param result 32 byte array to hold the result
  */
 /* DO NOT MODIFY THIS GENERATED FILE. INSTEAD CHANGE sdk/bpf/c/inc/sol/inc/blake3.inc AND RUN `cargo run --bin gen-headers` */
-#ifndef SAFE_SBFV2
-uint64_t sol_blake3(const SafeBytes *, int, const uint8_t *);
+#ifndef WICKANDBERGAMOT_SBFV2
+uint64_t sol_blake3(const WickandbergamotBytes *, int, const uint8_t *);
 #else
-typedef uint64_t(*sol_blake3_pointer_type)(const SafeBytes *, int, const uint8_t *);
-static uint64_t sol_blake3(const SafeBytes * arg1, int arg2, const uint8_t * arg3) {
+typedef uint64_t(*sol_blake3_pointer_type)(const WickandbergamotBytes *, int, const uint8_t *);
+static uint64_t sol_blake3(const WickandbergamotBytes * arg1, int arg2, const uint8_t * arg3) {
   sol_blake3_pointer_type sol_blake3_pointer = (sol_blake3_pointer_type) 390877474;
   return sol_blake3_pointer(arg1, arg2, arg3);
 }
