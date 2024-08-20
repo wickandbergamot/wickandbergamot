1813b4061a3356a1dc525c33838dee431f1126fb
Wick and Bergamot <sarahpoonn@gmail.com>
2024-08-20 08:20:02 +0800

    Update pubkey.h

--- sdk/bpf/c/inc/sol/pubkey.h
+++ sdk/bpf/c/inc/sol/pubkey.h
@@ -1,6 +1,6 @@
 #pragma once
 /**
- * @brief Safecoin Public key
+ * @brief Wickandbergamot Public key
  */
 
 #include <sol/types.h>
@@ -19,7 +19,7 @@ extern "C" {
  */
 typedef struct {
   uint8_t x[SIZE_PUBKEY];
-} SafePubkey;
+} WickandbergamotPubkey;
 
 /**
  * Prints the hexadecimal representation of a public key
@@ -27,11 +27,11 @@ typedef struct {
  * @param key The public key to print
  */
 /* DO NOT MODIFY THIS GENERATED FILE. INSTEAD CHANGE sdk/bpf/c/inc/sol/inc/pubkey.inc AND RUN `cargo run --bin gen-headers` */
-#ifndef SAFE_SBFV2
-void sol_log_pubkey(const SafePubkey *);
+#ifndef WICKANDBERGAMOT_SBFV2
+void sol_log_pubkey(const WickandbergamotPubkey *);
 #else
-typedef void(*sol_log_pubkey_pointer_type)(const SafePubkey *);
-static void sol_log_pubkey(const SafePubkey * arg1) {
+typedef void(*sol_log_pubkey_pointer_type)(const WickandbergamotPubkey *);
+static void sol_log_pubkey(const WickandbergamotPubkey * arg1) {
   sol_log_pubkey_pointer_type sol_log_pubkey_pointer = (sol_log_pubkey_pointer_type) 2129692874;
   sol_log_pubkey_pointer(arg1);
 }
@@ -44,7 +44,7 @@ static void sol_log_pubkey(const SafePubkey * arg1) {
  * @param two Second public key
  * @return true if the same
  */
-static bool SafePubkey_same(const SafePubkey *one, const SafePubkey *two) {
+static bool WickandbergamotPubkey_same(const WickandbergamotPubkey *one, const WickandbergamotPubkey *two) {
   for (int i = 0; i < sizeof(*one); i++) {
     if (one->x[i] != two->x[i]) {
       return false;
@@ -59,16 +59,16 @@ static bool SafePubkey_same(const SafePubkey *one, const SafePubkey *two) {
 typedef struct {
   const uint8_t *addr; /** Seed bytes */
   uint64_t len; /** Length of the seed bytes */
-} SafeSignerSeed;
+} WickandbergamotSignerSeed;
 
 /**
  * Seeds used by a signer to create a program address or passed to
  * sol_invoke_signed
  */
 typedef struct {
-  const SafeSignerSeed *addr; /** An array of a signer's seeds */
+  const WickandbergamotSignerSeed *addr; /** An array of a signer's seeds */
   uint64_t len; /** Number of seeds */
-} SafeSignerSeeds;
+} WickandbergamotSignerSeeds;
 
 /**
  * Create a program address
@@ -79,11 +79,11 @@ typedef struct {
  * @param program_address Program address created, filled on return
  */
 /* DO NOT MODIFY THIS GENERATED FILE. INSTEAD CHANGE sdk/bpf/c/inc/sol/inc/pubkey.inc AND RUN `cargo run --bin gen-headers` */
-#ifndef SAFE_SBFV2
-uint64_t sol_create_program_address(const SafeSignerSeed *, int, const SafePubkey *, SafePubkey *);
+#ifndef Wickandbergamot_SBFV2
+uint64_t sol_create_program_address(const WickandbergamotSignerSeed *, int, const WickandbergamotPubkey *, WickandbergamotPubkey *);
 #else
-typedef uint64_t(*sol_create_program_address_pointer_type)(const SafeSignerSeed *, int, const SafePubkey *, SafePubkey *);
-static uint64_t sol_create_program_address(const SafeSignerSeed * arg1, int arg2, const SafePubkey * arg3, SafePubkey * arg4) {
+typedef uint64_t(*sol_create_program_address_pointer_type)(const WickandbergamotSignerSeed *, int, const WickandbergamotPubkey *, WickandbergamotPubkey *);
+static uint64_t sol_create_program_address(const WickandbergamotSignerSeed * arg1, int arg2, const WickandbergamotPubkey * arg3, WickandbergamotPubkey * arg4) {
   sol_create_program_address_pointer_type sol_create_program_address_pointer = (sol_create_program_address_pointer_type) 2474062396;
   return sol_create_program_address_pointer(arg1, arg2, arg3, arg4);
 }
@@ -99,24 +99,24 @@ static uint64_t sol_create_program_address(const SafeSignerSeed * arg1, int arg2
  * @param bump_seed Bump seed required to create a valid program address
  */
 /* DO NOT MODIFY THIS GENERATED FILE. INSTEAD CHANGE sdk/bpf/c/inc/sol/inc/pubkey.inc AND RUN `cargo run --bin gen-headers` */
-#ifndef SAFE_SBFV2
-uint64_t sol_try_find_program_address(const SafeSignerSeed *, int, const SafePubkey *, SafePubkey *, uint8_t *);
+#ifndef WICKANDBERGAMOT_SBFV2
+uint64_t sol_try_find_program_address(const WickandbergamotSignerSeed *, int, const WickandbergamotPubkey *, WickandbergamotPubkey *, uint8_t *);
 #else
-typedef uint64_t(*sol_try_find_program_address_pointer_type)(const SafeSignerSeed *, int, const SafePubkey *, SafePubkey *, uint8_t *);
-static uint64_t sol_try_find_program_address(const SafeSignerSeed * arg1, int arg2, const SafePubkey * arg3, SafePubkey * arg4, uint8_t * arg5) {
+typedef uint64_t(*sol_try_find_program_address_pointer_type)(const WickandbergamotSignerSeed *, int, const WickandbergamotPubkey *, WickandbergamotPubkey *, uint8_t *);
+static uint64_t sol_try_find_program_address(const WickandbergamotSignerSeed * arg1, int arg2, const WickandbergamotPubkey * arg3, WickandbergamotPubkey * arg4, uint8_t * arg5) {
   sol_try_find_program_address_pointer_type sol_try_find_program_address_pointer = (sol_try_find_program_address_pointer_type) 1213221432;
   return sol_try_find_program_address_pointer(arg1, arg2, arg3, arg4, arg5);
 }
 #endif
 
-#ifdef SAFE_TEST
+#ifdef Wickandbergamot_TEST
 /**
  * Stub functions when building tests
  */
 #include <stdio.h>
 
 void sol_log_pubkey(
-  const SafePubkey *pubkey
+  const WickandbergamotPubkey *pubkey
 ) {
   printf("Program log: ");
   for (int i = 0; i < SIZE_PUBKEY; i++) {
